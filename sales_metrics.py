2df2db1f112d4d4e37da811e58fb76f29e7d7f5c
Srijan Bhattarai <srijanrbhattarai@gmail.com>
2025-05-21 19:46:14 -0400

    Add initial sales_metrics script

---
+++ sales_metrics.py
@@ -0,0 +1,13 @@
+# sales_metrics.py
+
+def calculate_daily_sales(sales):
+    """
+    Calculates the total sales for the day.
+    :param sales: list of numerical sales values
+    :return: total sales sum
+    """
+    return sum(sales)
+
+if __name__ == "__main__":
+    sample_sales = [100, 200, 150]
+    print("Total Sales:", calculate_daily_sales(sample_sales))
